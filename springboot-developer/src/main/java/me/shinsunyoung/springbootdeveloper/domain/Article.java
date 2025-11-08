566fbe44c088640816d5f5e4e1de46699387dbfb
hojinlee <infohojin@naver.com>
2025-11-08 16:43:32 +0900

    6장 들어가기 전 커밋

---
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
@@ -0,0 +1,10 @@
+package me.shinsunyoung.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = )
+}
