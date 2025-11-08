dcf6625cd436c68f8027548c52760eeedad486b7
hojinlee <infohojin@naver.com>
2025-11-08 20:14:34 +0900

    BlogApiController 수정

--- springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogApiContoller.java
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogApiContoller.java
@@ -3,13 +3,17 @@ package me.shinsunyoung.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.springbootdeveloper.domain.Article;
 import me.shinsunyoung.springbootdeveloper.dto.AddArticleRequest;
+import me.shinsunyoung.springbootdeveloper.dto.ArticleResponse;
 import me.shinsunyoung.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +25,14 @@ public class BlogApiContoller {
         Article saveArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(saveArticle);
+    }
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
