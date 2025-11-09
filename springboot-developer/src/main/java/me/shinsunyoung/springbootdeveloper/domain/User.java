80ac4a3f2acf1997f1b5afb6f83ff4b3cdd5a078
hojinlee <infohojin@naver.com>
2025-11-09 09:51:59 +0900

    8장 마무리 코드 구현 및 테스트

--- springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/User.java
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/User.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Table
+@Table(name = "users")
 @NoArgsConstructor
 @Getter
 @Entity
